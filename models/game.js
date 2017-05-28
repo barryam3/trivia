e9164d183cde8e0a6176a61addfb71fe0b697233
Barry McNamara <bamazap@hotmail.com>
2017-05-27 22:27:42 -0700

    Transitioning to round 2 works now.

--- models/game.js
+++ models/game.js
@@ -84,6 +84,7 @@ gameSchema.statics.getGame = function(uid, callback) {
 
 gameSchema.statics.askQuestion = function(uid, qid, callback) {
     var that = this;
+    // get the game so we can figure out what round we are in
     that.findOne({uid: uid}, function(err, game) {
         if (err) {
             callback({
@@ -91,11 +92,25 @@ gameSchema.statics.askQuestion = function(uid, qid, callback) {
             });
             return;
         }
-        var q_per_c = game.round == 'single' ? game.single[0].questions.length : game.double[0].questions.length;
+        var board = game.round == 'single' ? game.single : game.double;
+        // convert qid to two indexes
+        var q_per_c = board[0].questions.length;
         var c = Math.floor(qid/q_per_c).toString(); // category
         var v  = (qid % q_per_c).toString(); // value - 1
+        // count number of unasked questions
+        board[c].questions[v].asked = true; // set this one to asked since it might not be
+        var unasked_questions = board.reduce(function(t1, c) {
+            return t1 + c.questions.reduce(function(t2, q) {
+                return t2 + (q.asked ? 0 : 1);
+            }, 0);
+        }, 0);
+        console.log(unasked_questions);
+        // build update
         var updateObj = {};
         updateObj[game.round+"."+c+".questions."+v+".asked"] = true;
+        if (unasked_questions == 0) {
+            updateObj['round'] = game.round == 'single' ? 'double' : 'final';
+        }
         that.update({uid : uid}, {$set : updateObj}, callback);
     });
 };
