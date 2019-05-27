5420a571b30c5eaacd38fdb13a8510cec1965e86
Barry Anthony McNamara III <bamazap@hotmail.com>
2019-05-27 12:32:52 -0400

    Update start scripts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const bodyParser = require('body-parser');
 
 // Require routes
