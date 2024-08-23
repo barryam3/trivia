789007306188741eb67813d0c5b4bb282e00cf94
Barry McNamara <barryam3@alum.mit.edu>
2024-08-23 15:53:50 -0700

    Convert Scores from class to function component.

--- src/Elements/Scores.tsx
+++ src/Elements/Scores.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import { RouteComponentProps, withRouter } from "react-router-dom";
 
@@ -9,94 +9,84 @@ interface Props extends RouteComponentProps {
   contestants: Contestant[];
   uid: string;
   leader: boolean;
-  value: number|null;
+  value: number | null;
   multiplier: number;
 }
 
-class Scores extends Component<Props> {
-  state = {
-    contestants: this.props.contestants,
-  };
+const Scores: React.FC<Props> = (props) => {
+  const [state, setState] = useState({
+    contestants: props.contestants,
+  });
 
-  componentWillReceiveProps(nextProps: Props) {
-    this.setState({ contestants: nextProps.contestants });
-  }
+  const contestants = props.leader ? state.contestants : props.contestants;
 
-  updateScore = (key: number, diff: number) => {
+  const updateScore = (key: number, diff: number) => {
     return () => {
-      this.setState((prevState) => {
+      setState((prevState) => {
         const newState = JSON.parse(JSON.stringify(prevState));
         newState.contestants[key].score += diff;
         return prevState;
       });
-      Services.games.updateScore(this.props.uid, key, diff);
+      Services.games.updateScore(props.uid, key, diff);
     };
   };
 
-  render() {
-    return (
-      <div id="scores">
-        {(this.state.contestants || []).map((c, key) => (
-          <div key={key}>
-            {this.props.leader && (
-              <div className="buttons">
+  return (
+    <div id="scores">
+      {contestants.map((c, key) => (
+        <div key={key}>
+          {props.leader && (
+            <div className="buttons">
+              <button
+                type="button"
+                className="scorebutton"
+                style={{ backgroundColor: "red" }}
+                onClick={updateScore(key, -1)}
+              >
+                -
+              </button>
+              {props.value != null && (
                 <button
                   type="button"
                   className="scorebutton"
                   style={{ backgroundColor: "red" }}
-                  onClick={this.updateScore(key, -1)}
+                  onClick={updateScore(key, -props.multiplier * props.value)}
                 >
-                  -
+                  W
                 </button>
-                {this.props.value != null && (
-                  <button
-                    type="button"
-                    className="scorebutton"
-                    style={{ backgroundColor: "red" }}
-                    onClick={this.updateScore(
-                      key,
-                      -this.props.multiplier * this.props.value
-                    )}
-                  >
-                    W
-                  </button>
-                )}
-              </div>
-            )}
-            <div>
-              <div className="scorename">{c.name}</div>
-              <div className="scorescore">${c.score}</div>
+              )}
             </div>
-            {this.props.leader && (
-              <div className="buttons">
+          )}
+          <div>
+            <div className="scorename">{c.name}</div>
+            <div className="scorescore">${c.score}</div>
+          </div>
+          {props.leader && (
+            <div className="buttons">
+              <button
+                type="button"
+                className="scorebutton"
+                style={{ backgroundColor: "green" }}
+                onClick={updateScore(key, 1)}
+              >
+                +
+              </button>
+              {props.value != null && (
                 <button
                   type="button"
                   className="scorebutton"
                   style={{ backgroundColor: "green" }}
-                  onClick={this.updateScore(key, 1)}
+                  onClick={updateScore(key, props.multiplier * props.value)}
                 >
-                  +
+                  R
                 </button>
-                {this.props.value != null && (
-                  <button
-                    type="button"
-                    className="scorebutton"
-                    style={{ backgroundColor: "green" }}
-                    onClick={this.updateScore(
-                      key,
-                      this.props.multiplier * this.props.value
-                    )}
-                  >
-                    R
-                  </button>
-                )}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+              )}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default withRouter(Scores);
