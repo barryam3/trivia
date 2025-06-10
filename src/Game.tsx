b5da782a4efdf2fdbd63444a7ac91c8e3bc553e8
Barry McNamara <barryam3@alum.mit.edu>
2025-06-10 09:00:59 -0700

    Start team scores

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,12 +3,14 @@ import { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router";
 
 import Scores from "./Elements/Scores";
+import TeamScores from "./Elements/TeamScores";
 import services from "./services/index";
 import { SerialButton } from "./Elements/SerialButton";
 
 const Game: React.FC = () => {
   const location = useLocation();
   const leader = services.games.useLeader();
+  const { teams } = services.games.useGame();
 
   // Whenever a navigation happens in the leader view, make all follower views navigate.
   const navigate = useNavigate();
@@ -27,10 +29,14 @@ const Game: React.FC = () => {
   return (
     <div id="game">
       <SerialButton />
-      <div id="game-content">
-        <Outlet />
+      <div className="hstack flex1">
+        {teams && <TeamScores teamIndex={0} />}
+        <div id="game-content">
+          <Outlet />
+        </div>
+        {teams && <TeamScores teamIndex={1} />}
       </div>
-      <Scores />
+      {!teams && <Scores />}
     </div>
   );
 };
