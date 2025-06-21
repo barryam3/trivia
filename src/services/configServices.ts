b34f1354b5644a0d158050e2a657669b69c9768c
Barry McNamara <barryam3@alum.mit.edu>
2025-06-21 14:47:03 -0700

    Add documentation for buzzers

--- src/services/configServices.ts
+++ src/services/configServices.ts
@@ -1,11 +1,20 @@
 const CONFIG_KEY = "global/config";
 
 interface Config {
+  /*
+   * The pin mappings are the pins on the Arduino that are connected to the
+   * buzzer system. pinMappings[i] is the pin on the Arduino that is connected
+   * to the i-th contestant's buzzer.
+   */
   pinMappings: number[];
 }
 
 const DEFAULT_CONFIG = {
-  pinMappings: [38, 40, 42, 44, 46, 48, 50, 39, 41, 43, 45, 47, 49, 51],
+  /**
+   * The default pin mappings for the buzzer system. These are the pins the
+   * author uses on an Arduino Mega.
+   */
+  pinMappings: [38, 40, 42, 44, 46, 48, 50, 52, 39, 41, 43, 45, 47, 49, 51, 53],
 };
 
 function setConfig(config: Config) {
